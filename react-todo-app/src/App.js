5f183ae66585284ffea68c423de87644abcb66f6
khkang <alkalisummer@gmail.com>
2023-05-11 17:11:18 +0900

    to do list 모두 삭제 기능 추가

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -20,13 +20,18 @@ export default function App() {
     setTodoData(prev => [...prev, newTodo]);
     setValue("");
     
-  }, [value])
+  }, [value]);
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  }
 
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-5 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1 className="font-bold">To Do List</h1>
+          <button onClick={handleRemoveClick} className="text-xs">Delete All</button>
         </div>
         <Lists todoData={todoData} setTodoData={setTodoData}/>
         <Form value={value} setValue={setValue} handleSubmit={handleSubmit}/>
